bc24dcc1bc64109f9635ea720a2fbed0dd5f53c3
Lucca <lucca180@gmail.com>
2019-11-05 21:40:09 -0300

    Updates

---
+++ precache-manifest.13cd1b09ea41b3588180d6acb191e9bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1a8bd617fcced67a874524755aaef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "239408c229877c8b3112",
+    "url": "/static/css/main.22253894.chunk.css"
+  },
+  {
+    "revision": "13dc28deb19298ddf430",
+    "url": "/static/js/2.ff22b040.chunk.js"
+  },
+  {
+    "revision": "239408c229877c8b3112",
+    "url": "/static/js/main.6d386307.chunk.js"
+  },
+  {
+    "revision": "5f8b9230e0194306e9d2",
+    "url": "/static/js/runtime-main.6952224b.js"
+  }
+]);
